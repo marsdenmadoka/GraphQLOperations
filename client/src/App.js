d8adfc0f878c85652d62182dfb519377821bebdd
marsdenmadoka <marsmadoka98@gmail.com>
2020-07-09 23:23:56 +0300

    added react for client side

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+  return (
+    <div id="main">
+    <h1>madoka reading books</h1>
+    </div>
+  );
+}
+}
+export default App;
